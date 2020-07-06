43ef6f0c3685a8d5d25084fc5d2000258e87e34d
Alvin <alvin.farkhan@gmail.com>
2020-07-06 09:24:16 +0700

    shorten timeout

--- src/main/java/com/debrief2/pulsa/order/utils/rpc/RPCClient.java
+++ src/main/java/com/debrief2/pulsa/order/utils/rpc/RPCClient.java
@@ -29,7 +29,7 @@ public class RPCClient {
     params.channel(connection.createChannel());
     params.exchange("");
     params.routingKey(routingKey);
-    params.timeout(50000);
+    params.timeout(20000);
     RpcClient rpcClient = new RpcClient(params);
     String response  = rpcClient.stringCall(message);
     rpcClient.close();
